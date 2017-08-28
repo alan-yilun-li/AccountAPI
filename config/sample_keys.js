ea79f50dae8ccecb17cce3d592d1546a6dbf1565
Alan Li <alan.yilun.li@gmail.com>
2017-08-28 15:32:08 -0400

    Added sample for files in .gitignore

---
+++ config/sample_keys.js
@@ -0,0 +1,11 @@
+
+// Sample File to model keys.js that stores secret API keys and other private information.
+// This file is only to act as a reference keys.js which is in .gitignore and is not otherwise used.
+
+"use strict"
+
+module.exports = {
+
+  dbURL: 'sample_url_place_your_real_url_here'
+
+}
